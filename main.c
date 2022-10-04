c52199d9d7a1ac339aaeb9a673102188b57b07c5
Luis Adiel Martínez <luisadiel99@gmail.com>
2022-10-03 20:58:50 -0500

    Fix main file encoding
    Also make it less cumbersome, I guess?

--- main.c
+++ main.c
@@ -1,72 +1,63 @@
 #include "minidb.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
-#define FATAL(retval)                                          \
-do {                                                           \
-    if ((retval) != MINIDB_OK) {                               \
-        printf("Error: %s\n\n", minidb_get_error_str(retval)); \
-        minidb_close(&db);                                     \
-        exit(1);                                               \
-    }                                                          \
-} while (0)
+#define COMMAND_MAX_STRLEN 256
 
 #define prompt_string(text, outbuff)            \
 do {                                            \
     printf((text));                             \
     fflush(stdout);                             \
     fgets((outbuff), sizeof((outbuff)), stdin); \
-    outbuff[strlen((outbuff)) - 1] = '\0';      \
+    (outbuff)[strlen((outbuff)) - 1] = '\0';    \
 } while (0)
 
-#define prompt_int(text, intvar)    \
+#define prompt_int(text, intval)    \
 do {                                \
     char buff[20];                  \
-    prompt_string((text), buff);    \
-    sscanf_s(buff, "%d", (intvar)); \
+    prompt_string(text, buff);      \
+    sscanf_s(buff, "%d", (intval)); \
 } while (0)
 
-#define prompt_float(text, floatvar)  \
-do {                                  \
-    char buff[20];                    \
-    prompt_string((text), buff);      \
-    sscanf_s(buff, "%f", (floatvar)); \
+#define prompt_float(text, intval)  \
+do {                                \
+    char buff[20];                  \
+    prompt_string(text, buff);      \
+    sscanf_s(buff, "%f", (intval)); \
 } while (0)
 
 typedef struct Alumno
 {
-    char nombre[16];
-    int32_t ncontrol;
+    char nombre[32]; // 31 + \0
+    int ncontrol;
     float promedio;
 } Alumno;
 
 static void print_pretty_table(const Alumno *alumno)
 {
-    puts("+------------------+------------+----------+");
-    puts("|      Nombre      | N. Control | Promedio |");
-    puts("+------------------+------------+----------+");
-    printf("| %-16s | % 10d | %8.1f |\n", alumno->nombre, alumno->ncontrol, alumno->promedio);
-    puts("+------------------+------------+----------+");
+    puts("+-----------------------------------------------------+------------+----------+");
+    puts("|                       Nombre                        | N. Control | Promedio |");
+    puts("+-----------------------------------------------------+------------+----------+");
+    printf("| %-31s | % 10d | %8.1f |\n", alumno->nombre, alumno->ncontrol, alumno->promedio);
+    puts("+-----------------------------------------------------+------------+----------+");
 }
 
-static void select_dump_all_recursive(MiniDb *db, BinaryTreeNode *node)
+static void select_print_all_recursive(MiniDb *db, BinaryTreeNode *node)
 {
     if (node != NULL) {
-        select_dump_all_recursive(db, node->left);
+        select_print_all_recursive(db, node->left);
 
         Alumno alumno;
         MiniDbError error = minidb_select(db, node->key, &alumno);
         if (error != MINIDB_OK) {
-            printf("Panik! %s\n", minidb_error_get_str(error));
+            printf("PANIK! %s\n", minidb_error_get_str(error));
             return;
         }
 
         print_pretty_table(&alumno);
         puts("");
 
-        select_dump_all_recursive(db, node->right);
+        select_print_all_recursive(db, node->right);
     }
 }
 
@@ -75,33 +66,33 @@ int main(void)
     MiniDb db;
     MiniDbError error;
 
-    char command[255];
-    char archivo[255];
+    char command[COMMAND_MAX_STRLEN];
+    char filepath[COMMAND_MAX_STRLEN];
 
     // Preamble
-    printf("MiniDb x%zu bits\n\n", sizeof(int*) * 8);
+    printf("MiniDb x%zu bits.\n\n", sizeof(int *) * 8);
 
     while (1) {
         prompt_string("MiniDb > ", command);
 
-        if (strcmp(command, "salir") == 0 || strcmp(command, "exit") == 0) {
+        if (strcmp(command, "exit") == 0) {
             return 0;
-        } else if (strcmp(command, "nueva") == 0) {
-            prompt_string("Physical name: ", archivo);
+        } else if (strcmp(command, "new") == 0) {
+            prompt_string("Path: ", filepath);
 
-            int n_paginas;
-            prompt_int("Size: ", &n_paginas);
+            int size;
+            prompt_int("Size: ", &size);
 
             printf("Creando base de datos... ");
             fflush(stdout);
-            minidb_create(&db, archivo, sizeof(Alumno), n_paginas);
+            minidb_create(&db, filepath, sizeof(Alumno), size);
             puts("Ok!\n");
             fflush(stdout);
-        } else if (strcmp(command, "abrir") == 0) {
-            prompt_string("Physical name: ", archivo);
-            minidb_open(&db, archivo);
+        } else if (strcmp(command, "open") == 0) {
+            prompt_string("Physical name: ", filepath);
+            minidb_open(&db, filepath);
             if (db.file == NULL) {
-                printf("Error fatal: archivo no encontrado.\n");
+                printf("Error fatal: el archivo no existe o no se pudo abrir.\n");
                 exit(1);
             }
 
@@ -121,16 +112,16 @@ int main(void)
         break;
     }
 
-    const char *nombre_db = archivo;
-    for (int64_t i = (int64_t) strlen(archivo) - 1; i >= 0; i--) {
-        if (archivo[i] == '\\' || archivo[i] == '/') {
-            nombre_db = archivo + i + 1;
+    const char *db_name = filepath;
+    for (int64_t i = (int64_t) strlen(filepath) - 1; i >= 0; i--) {
+        if (filepath[i] == '\\' || filepath[i] == '/') {
+            db_name = filepath + i + 1;
             break;
         }
     }
 
     while (1) {
-        printf("MiniDb\\%s > ", nombre_db);
+        printf("MiniDb\\%s> ", db_name);
         fflush(stdout);
         fgets(command, sizeof(command), stdin);
         command[strlen(command) - 1] = '\0';
@@ -140,16 +131,16 @@ int main(void)
             break;
         } else if (strcmp(command, "dbinfo") == 0) {
             puts("== DATABASE INFO ==");
-            printf("Physical name : %s\n", archivo);
-            printf("Row Size      : %zu\n", db.row_size);
-            printf("Row Count     : %zu\n", db.row_count);
-            printf("Max Row Count : %zu\n", db.max_rows);
-            printf("Db Data Size  : %zu bytes\n", db.max_rows * db.row_size);
+            printf("Physical name : %s\n", db_name);
+            printf("Row Size      : %zu\n", db.header.row_size);
+            printf("Row Count     : %zu\n", db.header.row_count);
+            printf("Max Row Count : %zu\n", db.header.max_rows);
+            printf("Db Data Size  : %zu bytes\n", db.header.max_rows * db.header.row_size);
             puts("");
         } else if (strcmp(command, "select") == 0) {
             int ncontrol;
             prompt_int("N. control: ", &ncontrol);
-            puts(""); // A�adir espaciado
+            puts("");
 
             error = minidb_select(&db, ncontrol, &alumno);
             if (error != MINIDB_OK) {
@@ -160,9 +151,9 @@ int main(void)
             print_pretty_table(&alumno);
             printf("\n");
         } else if (strcmp(command, "select *") == 0) {
-            select_dump_all_recursive(&db, db.index.root);
+            select_print_all_recursive(&db, db.index.root);
         } else if (strcmp(command, "insert") == 0) {
-            prompt_string("Nombre[16]: ", alumno.nombre);
+            prompt_string("Nombre[31]: ", alumno.nombre);
             prompt_int("N. control: ", &alumno.ncontrol);
             prompt_float("Promedio:   ", &alumno.promedio);
 
@@ -174,7 +165,7 @@ int main(void)
 
             puts("Tupla insertada correctamente\n");
         } else if (strcmp(command, "update") == 0) {
-            prompt_string("Nombre[16]: ", alumno.nombre);
+            prompt_string("Nombre[31]: ", alumno.nombre);
             prompt_int("N. control: ", &alumno.ncontrol);
             prompt_float("Promedio:   ", &alumno.promedio);
 
